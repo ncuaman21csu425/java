943742c82b9325ecbfde4d1845ac7fce4261549e
Aman yadav <96002479+ncuaman21csu425@users.noreply.github.com>
2022-03-09 13:59:12 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+public class sum {
+ public static void main (String []args) {
+     int num1=5;
+     int num2=10;
+     int add =num1 +num2; 
+     int mul=num1*num2;
+     int sub=num1-num2;
+     int divide=num2/num1;
+     System.out.println("add:"+add);
+     System.out.println("mul:"+mul);
+     System.out.println("sub:"+sub);
+     System.out.println("divide:"+divide);
+
+ }  
+}
